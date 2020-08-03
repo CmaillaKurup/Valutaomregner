ce68be51514da191d1067d177d7fdf2f61a4252e
cami670v <cami670v@ZBC-AA-UBUY12>
2020-08-03 13:44:53 +0200

    Add project files.

---
+++ Valutaomregner/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Valutaomregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+            double usDollars = 0.16;
+            double britisk = 0.12;
+            double euro = 0.13;
+            double svensk = 1.38;
+
+            Console.WriteLine("US Dollars = " + (input + usDollars));
+            Console.WriteLine("Britiske Pund = " + (input + britisk));
+            Console.WriteLine("Euro = " + (input+ euro));
+            Console.WriteLine("Svensk = " + (input + svensk));
+        }
+    }
+}
